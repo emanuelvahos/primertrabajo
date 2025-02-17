3ab5b3aa4fa28cdf9c71848725b178ce3b13e962
emanuelvahos <emanuelvahosv@gmail.com>
2025-02-17 07:12:58 -0500

    Update talentotech_app.py

--- talentotech_app.py
+++ talentotech_app.py
@@ -1,1 +1,8 @@
-
+#1. configuración inicial de la aplicación 
+st.set_page_config(
+  page_tityle="Dashboard Interactivo",
+  page_icon="📊",
+  layout="wide"
+)
+st.title("📊 Dashboard Interactivo con Streamlit")
+st.sidebar.title("🔍 Ociones de navegación")
