6661d82712e6411e1efd41ecde1e2e0dbdac403c
emanuelvahos <emanuelvahosv@gmail.com>
2025-02-17 09:00:51 -0500

    Update Archivo.py

--- Archivo.py
+++ Archivo.py
@@ -36,8 +36,8 @@ with col2:  # Columna central para el título
     )
     st.markdown("<h1 class='titulo'>Agroindustria en Colombia</h1>", unsafe_allow_html=True)
 
-# Imagen de encabezado (opcional)
-image = Image.open("ruta_de_la_imagen.jpg")  # Reemplaza con la ruta de tu imagen
+# Imagen de encabezado
+image = Image.open("https://s3.amazonaws.com/img.pymas.com.co/_600xAUTO_crop_center-center/agroindustria-en-colombia.png")
 st.image(image, use_column_width=True)
 
 
