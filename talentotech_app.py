3555d07ce1618bd095375c338ece49fb4b2f8bba
emanuelvahos <emanuelvahosv@gmail.com>
2025-02-17 07:13:15 -0500

    Update talentotech_app.py

--- talentotech_app.py
+++ talentotech_app.py
@@ -1,3 +1,11 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy as sp
+import seaborn as sns
+import plotly.express as px
+import datetime
 #1. configuración inicial de la aplicación 
 st.set_page_config(
   page_tityle="Dashboard Interactivo",
